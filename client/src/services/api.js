7de3936936c92f531816f28c22c258d44b100c1c
Namit2924 <namitdhangar5@gmail.com>
2026-03-23 12:15:08 +0530

    update frontend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://roomy-backend-1hsr.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
